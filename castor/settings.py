597e330a7a3018110331243879e44be16196c74f
Paris Kasidiaris <paris@kasidiaris.com>
2022-11-22 13:00:52 +0000

    Improve Docker setup

--- castor/settings.py
+++ castor/settings.py
@@ -1,8 +1,11 @@
 import sec
+from . import version
 
 
-DOCKER_HOST = sec.load("docker_host")
+_POETRY_VERSION = version.get_poetry_version()
+
+DOCKER_HOST = sec.load("docker_host", "unix:///var/run/docker.sock")
 SENTRY_DSN = sec.load("sentry_dsn")
 CASTOR_ENVIRONMENT = sec.load("castor_environment")
-CASTOR_RELEASE = sec.load("castor_release")
+CASTOR_RELEASE = sec.load("castor_release", _POETRY_VERSION)
 CASTOR_WEBHOOK_URL = sec.load("castor_webhook_url")
